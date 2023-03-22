2363b4cf0cd1a8956d98cae4aeaf6364fdfffd5f
yangteng@class100.com <Ybjy123>
2023-03-22 17:09:39 +0800

    feature: 添加自增id的基础model

--- model.go
+++ model.go
@@ -3,13 +3,19 @@ package easy
 import "time"
 
 type BaseModel struct {
-	Id        int64     `xorm:"pk notnull comment('主键')"`
-	CreatedAt time.Time `xorm:"datetime created notnull default('2023-01-01 00:00:00') comment('创建时间')"`
+	Id        int64     `xorm:"pk notnull comment('主键')" bson:"id"`
+	CreatedAt time.Time `xorm:"datetime created notnull default('2023-01-01 00:00:00') comment('创建时间')" bson:"created_at"`
+}
+
+type AutoIncrModel struct {
+	Id        int64     `xorm:"pk autoincr notnull comment('主键')" bson:"id"`
+	CreatedAt time.Time `xorm:"datetime created notnull default('2023-01-01 00:00:00') comment('创建时间')" bson:"created_at"`
 }
 
 type Model interface {
 	PK() int64
 	SetPK(pk int64)
+	UseSnowflakeIdAsDefault() bool
 }
 
 func (bm *BaseModel) PK() int64 {
@@ -18,4 +24,20 @@ func (bm *BaseModel) PK() int64 {
 
 func (bm *BaseModel) SetPK(pk int64) {
 	bm.Id = pk
+}
+
+func (bm *BaseModel) UseSnowflakeIdAsDefault() bool {
+	return true
+}
+
+func (am *AutoIncrModel) PK() int64 {
+	return am.Id
+}
+
+func (am *AutoIncrModel) SetPK(pk int64) {
+	am.Id = pk
+}
+
+func (am *AutoIncrModel) UseSnowflakeIdAsDefault() bool {
+	return false
 }
