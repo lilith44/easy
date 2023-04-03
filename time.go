b6e06de0bf5b50519580c3c831638d2f06c286b9
yangteng@class100.com <Ybjy123>
2023-04-03 17:04:37 +0800

    feature: 添加自定义time类型

--- time.go
+++ time.go
@@ -18,7 +18,7 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 		return nil
 	}
 
-	tm, err := time.Parse(`"`+DateTime+`"`, string(data))
+	tm, err := time.ParseInLocation(`"`+DateTime+`"`, string(data), time.Local)
 	if err != nil {
 		return err
 	}
@@ -51,40 +51,13 @@ func (t Time) MarshalText() ([]byte, error) {
 }
 
 func (t *Time) UnmarshalText(data []byte) error {
-	tm, err := time.Parse(`"`+DateTime+`"`, string(data))
+	tm, err := time.ParseInLocation(`"`+DateTime+`"`, string(data), time.Local)
 	if err != nil {
 		return err
 	}
 
 	*t = NewTime(tm)
 	return nil
-}
-
-func (t *Time) UnmarshalBSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-
-	tm, err := time.Parse(`"`+DateTime+`"`, string(data))
-	if err != nil {
-		return err
-	}
-
-	*t = NewTime(tm)
-	return nil
-}
-
-func (t Time) MarshalBSON() ([]byte, error) {
-	tm := t.Time()
-	if y := tm.Year(); y < 0 || y >= 10000 {
-		return nil, errors.New("Time.MarshalBSON: year outside of range [0,9999]")
-	}
-
-	b := make([]byte, 0, len(DateTime)+2)
-	b = append(b, '"')
-	b = tm.AppendFormat(b, DateTime)
-	b = append(b, '"')
-	return b, nil
 }
 
 func (t Time) Time() time.Time {
